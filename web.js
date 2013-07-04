5a0003dae717a46884e0b8de1cf23f80f0bbb774
idtree <mmidtree@gmail.com>
2013-07-04 21:41:19 +0000

    read index.html from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var index_content = fs.readFileSync('index.html');
+  response.send(index_content.toString());
 });
 
 var port = process.env.PORT || 5000;
